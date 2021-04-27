1fbaf2b4793cbff8a94fc644ae5fe923b9ab2232
Juan Ahumada <juan.ahumada94@gmail.com>
2021-04-27 08:46:01 -0500

    Flashlight function

---
+++ Assets/flashLight.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class flashLight : MonoBehaviour
+{
+    public void SetFlashLight(bool enabled){
+        Debug.Log($"Flashlight: {enabled}");
+        CameraDevice.Instance.SetFlashTorchMode(enabled);
+    }
+}
